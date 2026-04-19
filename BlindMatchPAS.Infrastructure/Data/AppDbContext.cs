f0b6d6d24820222a2bdfb2b443645046224672ce
Sdinujaya <sithikadinujaya123@gmail.com>
2026-04-19 14:30:17 +0530

    feat: add identity models, db context, and role seeding

---
+++ BlindMatchPAS.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BlindMatchPAS.Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+
+namespace BlindMatchPAS.Infrastructure.Data
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
